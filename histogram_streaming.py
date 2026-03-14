7da5f89fc3b1fa604ce067b11e01c998f2c469a5
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-03-14 21:59:27 +0000

    Delta t vs. Phase Density Histogram Streaming
    New version of delta t vs. phase density histogram which allows for streaming data

---
+++ histogram_streaming.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob 
+from matplotlib.colors import LogNorm
+
+#data initialisation
+directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\255 to 256 h\*"
+files = glob.glob(directory)
+#files = files[:1]
+
+files.sort(
+    key=lambda f: 
+    int(f.rsplit('part', 1)[1]
+        .replace('.csv', ''))
+) 
+
+fig, ax = plt.subplots()
+
+for file in files:
+    print(file)
+    df = pd.read_csv(file, usecols=["phase_deg", "q_pC", "d_time_s"])
+    df.dropna(inplace=True)
+
+    #positive times only
+    df_pos = df[df["d_time_s"] > 0]
+
+    #phase-time bins
+    df["d_time_s"] = df["d_time_s"].round(7) #rounds d_time to 7 decimal places (0.1x micro range)
+    df["phase_deg_rounded"] = df["phase_deg"].round().astype(int) #rounding phase to nearest degree
+
+    df_grouped = (df.groupby(["phase_deg_rounded", "d_time_s"]) .size()).reset_index(name = "count")
+
+    #plot 
+    scatter = ax.scatter(
+        df_grouped["phase_deg_rounded"],
+        df_grouped["d_time_s"],
+        c=df_grouped["count"],
+        norm=LogNorm(vmin=1, vmax=200),
+        cmap="plasma",
+        marker = '.',
+        s=10,
+        alpha=0.6
+    )
+
+plt.yscale("log")
+ax.set_xlabel("Phase (deg)")
+ax.set_ylabel("Δt (s) [log scale]")
+ax.set_title("Phase vs. Δt Density Histogram")
+fig.colorbar(scatter, ax=ax, label="PD Counts [log scale]") 
+
+plt.tight_layout()
+plt.show()
