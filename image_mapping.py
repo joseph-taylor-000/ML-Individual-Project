9199521f4e89bc4b7bb27fb63cddb15e3c915e08
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-03-15 17:50:22 +0000

    +Multiple file functionality

--- image_mapping.py
+++ image_mapping.py
@@ -5,30 +5,32 @@ from matplotlib.colors import LogNorm
 import glob
 import os
 
-directory = r"M:\OneDrive - The University of Manchester\ML_dataset\S4.4_Sample with usual arrangement\Processed images_0 to 96 hours\100325\diff_10 14 22 11 .jpg"
-img = mplimg.imread(directory)
-print(img)
-threshold = 80 #white zone
-
-y, x = np.where(img > threshold) 
-#img is 2D array of intensity values corresponding to position. 
-#select values bright enough to indicate discharge
-
-intensity = img[y, x] #list of all intensity values
-
-
+directory = r"M:\OneDrive - The University of Manchester\ML_dataset\S4.4_Sample with usual arrangement\Processed images_0 to 96 hours\100325\*"+".jpg"
+files = glob.glob(directory)
+#files = files[:1]
 fig, ax = plt.subplots()
 
-scatter = ax.scatter(
-        x,
-        y,
-        c=intensity,
-        norm=LogNorm(vmin=1, vmax=255),
-        cmap="plasma",
-        marker = '.',
-        s=10,
-        alpha=0.6
-    )
+for file in files:
+    img = mplimg.imread(file)
+    print(img)
+    threshold = 80 #white zone
+
+    y, x = np.where(img > threshold) 
+    #img is 2D array of intensity values corresponding to position. 
+    #select values bright enough to indicate discharge
+
+    intensity = img[y, x] #list of all intensity values
+
+    scatter = ax.scatter(
+            x,
+            y,
+            c=intensity,
+            norm=LogNorm(vmin=50, vmax=255),
+            cmap="plasma",
+            marker = '.',
+            s=10,
+            alpha=0.6
+        )
 
 
 ax.set_xlabel("X-position (pixel)")
