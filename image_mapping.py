fd7ec8f8a54c6996897039dbbf92fbacb1e04d56
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-03-16 13:12:54 +0000

    +User defined time period for analysis

--- image_mapping.py
+++ image_mapping.py
@@ -5,15 +5,39 @@ from matplotlib.colors import LogNorm
 import glob
 import os
 
-directory = r"M:\OneDrive - The University of Manchester\ML_dataset\S4.4_Sample with usual arrangement\Processed images_0 to 96 hours\100325\*"+".jpg"
+directory = r"M:\OneDrive - The University of Manchester\ML_dataset\S4.4_Sample with usual arrangement\Processed images_0 to 96 hours\100325\0 h - 2h\diff_10*"+".jpg"
 files = glob.glob(directory)
-#files = files[:1]
+files = files[:227]
 fig, ax = plt.subplots()
 
-for file in files:
+#period = input("Enter desired period for analysis: | format(hr/min/sec): 00 00 00")
+period = "01 00 00" #hardset
+period_sec = (int(period.rsplit(" ", 2)[0])*3600)+(int(period.rsplit(" ", 2)[1])*60)+(int(period.rsplit(" ", 2)[2])) #repeated rsplit inefficient
+
+file_time_1 = files[0].rsplit("diff_10", 1)[1].replace(" .jpg", '')
+file_time_sec_1 = (int(file_time_1.rsplit(" ", 2)[0])*3600)+(int(file_time_1.rsplit(" ", 2)[1])*60)+(int(file_time_1.rsplit(" ", 2)[2]))
+
+file_times = [file_time_sec_1, 0]
+i = len(files)-1
+
+for file in files[:i]:
+    print(file)
+    print(files.index(file))
+
+    file_time = file.rsplit("diff_10", 1)[1].replace(" .jpg", '')
+    file_time_sec = (int(file_time.rsplit(" ", 2)[0])*3600)+(int(file_time.rsplit(" ", 2)[1])*60)+(int(file_time.rsplit(" ", 2)[2]))
+    file_times[1] = file_time_sec
+    if (file_times[1]-file_times[0]>= period_sec):
+        i = files.index(file)
+        break
+
+#light detection
+threshold = 50 #white zone
+
+for file in files[:i]:
     img = mplimg.imread(file)
-    print(img)
-    threshold = 80 #white zone
+    #print(img)
+    
 
     y, x = np.where(img > threshold) 
     #img is 2D array of intensity values corresponding to position. 
@@ -25,7 +49,7 @@ for file in files:
             x,
             y,
             c=intensity,
-            norm=LogNorm(vmin=50, vmax=255),
+            norm=LogNorm(vmin=1, vmax=255),
             cmap="plasma",
             marker = '.',
             s=10,
