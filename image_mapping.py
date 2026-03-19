7f4c43901b95096a8accef9b9cc4536c552077a3
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-03-19 17:29:07 +0000

    +improved efficiency + graph colour for clarity

--- image_mapping.py
+++ image_mapping.py
@@ -5,17 +5,18 @@ from matplotlib.colors import LogNorm
 import glob
 import os
 
-directory = r"M:\OneDrive - The University of Manchester\ML_dataset\S4.4_Sample with usual arrangement\Processed images_0 to 96 hours\100325\0 h - 2h\diff_10*"+".jpg"
+directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\LIGHT IMAGES\S4.4_255to256h\*"+".jpg"
 files = glob.glob(directory)
-files = files[:227]
 fig, ax = plt.subplots()
 
 #period = input("Enter desired period for analysis: | format(hr/min/sec): 00 00 00")
 period = "01 00 00" #hardset
-period_sec = (int(period.rsplit(" ", 2)[0])*3600)+(int(period.rsplit(" ", 2)[1])*60)+(int(period.rsplit(" ", 2)[2])) #repeated rsplit inefficient
+period = period.rsplit(" ", 2)
+period_sec = (int(period[0])*3600)+(int(period[1])*60)+(int(period[2])) 
 
-file_time_1 = files[0].rsplit("diff_10", 1)[1].replace(" .jpg", '')
-file_time_sec_1 = (int(file_time_1.rsplit(" ", 2)[0])*3600)+(int(file_time_1.rsplit(" ", 2)[1])*60)+(int(file_time_1.rsplit(" ", 2)[2]))
+file_time_1 = files[0].rsplit("diff_30", 1)[1].replace(" .jpg", '')
+file_time_1 = file_time_1.rsplit(" ", 2)
+file_time_sec_1 = (int(file_time_1[0])*3600)+(int(file_time_1[1])*60)+(int(file_time_1[2]))
 
 file_times = [file_time_sec_1, 0]
 i = len(files)-1
@@ -24,15 +25,16 @@ for file in files[:i]:
     print(file)
     print(files.index(file))
 
-    file_time = file.rsplit("diff_10", 1)[1].replace(" .jpg", '')
-    file_time_sec = (int(file_time.rsplit(" ", 2)[0])*3600)+(int(file_time.rsplit(" ", 2)[1])*60)+(int(file_time.rsplit(" ", 2)[2]))
+    file_time = file.rsplit("diff_30", 1)[1].replace(" .jpg", '')
+    file_time = file_time.rsplit(" ", 2)
+    file_time_sec = (int(file_time[0])*3600)+(int(file_time[1])*60)+(int(file_time[2]))
     file_times[1] = file_time_sec
     if (file_times[1]-file_times[0]>= period_sec):
         i = files.index(file)
         break
 
 #light detection
-threshold = 50 #white zone
+threshold = 90 #white zone
 
 for file in files[:i]:
     img = mplimg.imread(file)
@@ -49,18 +51,18 @@ for file in files[:i]:
             x,
             y,
             c=intensity,
-            norm=LogNorm(vmin=1, vmax=255),
-            cmap="plasma",
+            norm=LogNorm(vmin=threshold, vmax=intensity.max()),
+            cmap="plasma", #gist_rainbow for greater contrast between all regions, plasma for clear high/low regions
             marker = '.',
             s=10,
             alpha=0.6
         )
 
-
+ax.set_facecolor((0, 0, 0))
 ax.set_xlabel("X-position (pixel)")
 ax.set_ylabel("Y-position (pixel)")
 ax.set_title("Light Intensity Plot")
 fig.colorbar(scatter, ax=ax, label="Light Intensity [log scale]") 
 
 plt.tight_layout()
-plt.show()
+plt.show() 
