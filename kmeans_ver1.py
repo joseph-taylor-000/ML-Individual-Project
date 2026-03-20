60206381786db94542bcbe6801fc85fd1c65dde0
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-03-20 12:03:21 +0000

    improved single file kmeans plot

--- kmeans_ver1.py
+++ kmeans_ver1.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
 #dataset initialisation
-directory = r"M:\OneDrive - The University of Manchester\Project_Backup\Individual Project\kmeans\PDdata2.csv"
+directory = r"M:\OneDrive - The University of Manchester\ML_Individual_Project\clusters_255_anomaly\HDBSCAN_cluster_-1.txt"
 dataset = pd.read_csv(directory)
-data_pandas = dataset[["time_s", "dq_pC"]] #select desired fields from dataframe
+data_pandas = dataset[["phase_deg", "q_pC"]] #select desired fields from dataframe
 dataset.dropna(inplace=True)
 data = data_pandas.to_numpy()
 
@@ -56,11 +57,35 @@ kmeans = KMeans(n_clusters=K_ideal, n_init=10)
 predictions = kmeans.fit_predict(data)
 centroids = kmeans.cluster_centers_
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.xlabel('Time (s)')
+plt.figure(figsize=(12, 5))
+
+scatter = plt.scatter(
+    data[:,0],      
+    data[:,1],       
+    c=predictions,   
+    cmap="tab10",            
+    s=10,
+    alpha=0.6,   
+    rasterized = True,
+    marker = ',' 
+
+)
+
+cmap = plt.get_cmap("tab10")  
+colours = []
+for i in range(K_ideal):
+    colour = scatter.cmap(scatter.norm(i))
+    colours.append(colour)
+
+handles = []
+for i in range(K_ideal):
+    patch = mpatches.Patch(color=colours[i], label=f"Cluster {i}")
+    handles.append(patch)
+
+plt.legend(handles=handles, title="Clusters", loc="lower right")
+plt.xlabel('Phase (deg)')
 plt.ylabel('Charge (pC)')
-plt.title('Partial Discharges over Time')
+plt.title('K-means Clustering: Phase Resolved Partial Discharge')
 
 #predict which cluster each data point will belong to (data[a] is one data point (all columns on row a))
 for i in range(0, K_ideal):
