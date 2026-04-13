e7743f989286504bb8a35fa847f5a8473bd12d4f
joseph-taylor-000 <jrptaylor000@gmail.com>
2026-04-13 21:00:37 +0100

    Improved density-time plot: +RMS value +Second resolution density method +Updated README_time_density

--- README_time_density.txt
+++ README_time_density.txt
@@ -5,22 +5,32 @@ Required libraries:
 matplotlib.pyplot: graphical plotting library for python.
 pandas: data processing library for python.
 numpy: math library for python.
+glob: file directory library
 
 #data initialisation
-* create pandas dataframe from csv file 
+* select directory
+* load directory into files 
 * drop NaN rows from df
 * optionally filter magnitude ranges
 
 #grouping
-* create dataframe column for microsecond time intervals
-* create new dataframe with time intervals, 
-  and the corresponding PD count values for each interval
-* sort the dataframe in ascending order
-* new dataframe created for plotting with .reset_index() adding a separate column for the index.
-* time intervals restored to original values.
+* create dataframe column for rounded phase degrees
+* create new dataframe with columns:
+>>time_us as the index 
+>>the corresponding count values for each phase group 
+>>the total charge for that group in another
+>>the count value of only positive magnitude events
+>>the total charge of positive events
+>>the count value of only negative magnitude events
+>>the total charge value of negative events
 
-#plot
-single axis plot:
-x-axis: time
-y-axis: partial discharge counts
+* new dataframe created for plotting with .reset_index() adding a separate column from time_us for the index.
+* RMS calculated for all values, only positive values, only negative values calculated
+* add file dataframe to plot
+
+#plot - 3 plots, one for all data, one for positive, one for negative
+twin axis plots: 
+ax1: density vs. time, 
+ax2: RMS PD magnitude vs. time
+
 
--- density_time.py
+++ density_time.py
@@ -1,42 +1,140 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import glob as glob
 import numpy as np
 
-directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\0 to 1h\Unit1.1_Qmin0pC_QpC_PhDeg_VkV_0.000_3600.400s_part01.csv"
-df = pd.read_csv(directory, usecols=["q_pC", "time_s"])
-df.dropna(inplace=True)
+
+#data initialisation
+directory_val = 3 
+
+#directory selection
+if directory_val == 1:
+    directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\0 to 1h\*" #0-1hr
+elif directory_val == 2:
+    directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\252 to 253 h\*" #252 to 253hr
+elif directory_val == 3:
+    directory = r"M:\OneDrive - The University of Manchester\ML_dataset\New datasets_Sample S4.4\255 to 256 h\*" #255 to 256hr
+
+files = glob.glob(directory)
+#files = files[:10]
+
+#figure set-up
+fig1, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+
+fig2, ax3 = plt.subplots()
+ax4 = ax3.twinx()
+
+fig3, ax5 = plt.subplots()
+ax6 = ax5.twinx()
+
+for file in files:
+    print(file)
+
+    df = pd.read_csv(file, usecols=["time_s", "q_pC"])
+    df.dropna(inplace=True)
+
 #df = df[(df["q_pC"] >= 5) | (df["q_pC"] <= -5)] #optional filters
 #df = df[(df["q_pC"] <= 5) & (df["q_pC"] >= -5)]
 #df = df[(df["q_pC"] <= 4) & (df["q_pC"] >= 3)]
 
-df["time_us"] = (df["time_s"] * 1e6).astype(int) #rounds to microsecond resolution of groupings
+    df["time_us"] = (df["time_s"] * 1e0).astype(int) #rounds to microsecond resolution of groupings
 
-density_time = (
-    df["time_us"]
-    .value_counts() #number of magnitude values with same time value
-    .sort_index() #sort index for ascending time values
+    density_time = (
+        df.groupby("time_us") 
+            .agg( #aggregate functions
+                counts = ("q_pC", "size"), ##PD counts at time 
+                q_pC_sq_total=("q_pC", lambda x: (x**2).sum()), #sum of PD counts squared (for RMS)
+                q_pC_sq_total_pos = ("q_pC", lambda x: ((x[x > 0])**2).sum()), #positive sum of squares - sum x where x > 0
+                q_pC_sq_total_neg = ("q_pC", lambda x: ((x[x < 0])**2).sum()), #negative sum of squares- sum x where x < 0
+                counts_pos = ("q_pC", lambda x: (x > 0).sum()), #positve values count - sum bool where x > 0
+                counts_neg = ("q_pC", lambda x: (x < 0).sum()) #negative values count - sum bool where x < 0
+            )
     )
 
-density_time_df = density_time.reset_index()
-density_time_df.columns = ["time_intervals", "density"]
-density_time_df["time_intervals"] = density_time_df["time_intervals"]/1e6
+    density_time_df = density_time.reset_index() #separate time from index
+    density_time_df["q_pC_rms"] = np.sqrt(density_time_df["q_pC_sq_total"]/density_time_df["counts"].where(density_time_df["counts"] > 0))
+    density_time_df["q_pC_rms_pos"] = np.sqrt(density_time_df["q_pC_sq_total_pos"]/density_time_df["counts_pos"].where(density_time_df["counts_pos"] > 0))
+    density_time_df["q_pC_rms_neg"] = -(np.sqrt(density_time_df["q_pC_sq_total_neg"]/density_time_df["counts_neg"].where(density_time_df["counts_neg"] > 0)))
+    density_time_df["time_us"] = density_time_df["time_us"]*1e-0
 
 
-#plot
-plt.figure(figsize=(12, 5))
-fig, ax1 = plt.subplots()
+    #plot 1
+    ax1.scatter(
+        density_time_df["time_us"],
+        density_time_df["counts"],
+        c="blue",
+        s=10,
+        alpha=0.6
+    )
 
-ax1.scatter(
-    density_time_df["time_intervals"],
-    density_time_df["density"],
-    s=10,
-    alpha=0.6
-)
+    ax2.scatter(
+        density_time_df["time_us"],
+        density_time_df["q_pC_rms"],
+        marker = 'x',
+        c='red',
+        s=10,
+        alpha=0.6
+    )
 
-ax1.set_yscale("log")
+    #plot 2
+    ax3.scatter(
+            density_time_df["time_us"],
+            density_time_df["counts_pos"],
+            c="blue",
+            s=10,
+            alpha=0.6
+        )
+        
+    ax4.scatter(
+            density_time_df["time_us"],
+            density_time_df["q_pC_rms_pos"],
+            marker = 'x',
+            c='red',
+            s=10,
+            alpha=0.6
+        )
+    #plot 3
+    ax5.scatter(
+            density_time_df["time_us"],
+            density_time_df["counts_neg"],
+            c="blue",
+            s=10,
+            alpha=0.6
+        )
+        
+    ax6.scatter(
+            density_time_df["time_us"],
+            density_time_df["q_pC_rms_neg"],
+            marker = 'x',
+            c='red',
+            s=10,
+            alpha=0.6
+        )
 
-plt.xlabel("Time (s)")
-plt.ylabel("Partial Discharge Counts")
-plt.title("Time-Discharge Density")
-plt.tight_layout()
+#plot 1
+ax1.set_title("Time-Discharge Density: All Events")
+ax1.set_xlabel("Time (seconds)")
+ax1.set_ylabel("Partial Discharge Counts")
+ax2.set_ylabel("Discharge Magnitude Root Mean Squared")
+fig1.tight_layout()
+
+
+#plot 2
+ax3.set_title("Time-Discharge Density: Positive Events")
+ax3.set_xlabel("Time (seconds)")
+ax3.set_ylabel("Partial Discharge Counts")
+ax4.set_ylabel("Discharge Magnitude Root Mean Squared")
+fig2.tight_layout()
+
+
+#plot 3
+ax5.set_title("Time-Discharge Density: Negative Events")
+ax5.set_xlabel("Time (seconds)")
+ax5.set_ylabel("Partial Discharge Counts")
+ax6.set_ylabel("Discharge Magnitude Root Mean Squared")
+fig3.tight_layout()
+
+
 plt.show()
+
